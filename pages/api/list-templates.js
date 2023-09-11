aba604e8ad47e0b25c97f15a97038a622e11493f
Kumar Harsh <kharsh39@gmail.com>
2023-09-11 17:59:04 +0530

    Simplify templates logic

--- pages/api/list-templates.js
+++ pages/api/list-templates.js
@@ -1,31 +1,17 @@
 import * as DropboxSign from "@dropbox/sign";
-
-
+const templateApi = new DropboxSign.TemplateApi();
 export default function handler(req, res) {
 
-
-    const accountApi = new DropboxSign.AccountApi();
-    accountApi.username = process.env.DROPBOX_SIGN_API_KEY;
-    const result = accountApi.accountGet(undefined, process.env.DROPBOX_SIGN_ACCOUNT_EMAIL_ADDRESS);
+    templateApi.username = process.env.DROPBOX_SIGN_API_KEY;
+    const result = templateApi.templateList();
 
     result.then(response => {
-
-        const accountId = response.body.account.accountId
-        const templateApi = new DropboxSign.TemplateApi();
-        templateApi.username = process.env.DROPBOX_SIGN_API_KEY;
-        const result = templateApi.templateList(accountId);
-
-        result.then(response => {
-            res.status(200).json(response.body.templates.map(template =>
-                ({
-                    id: template.templateId,
-                    name: template.title
-                })
-            ))
-        }).catch(error => {
-            console.log("Exception when calling Dropbox Sign API:");
-            console.log(error.body);
-        });
+        res.status(200).json(response.body.templates.map(template =>
+            ({
+                id: template.templateId,
+                name: template.title
+            })
+        ))
     }).catch(error => {
         console.log("Exception when calling Dropbox Sign API:");
         console.log(error.body);
